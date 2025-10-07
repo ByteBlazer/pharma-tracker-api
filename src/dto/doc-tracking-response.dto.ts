70fcdd516090c1ead475e5426d3cef571531bb00
Sujith George <sujith@byteblazer.com>
2025-10-07 11:32:24 +0530

    tracking

---
+++ src/dto/doc-tracking-response.dto.ts
@@ -0,0 +1,16 @@
+export class DocTrackingResponseDto {
+  success: boolean;
+  message: string;
+  status?: string;
+  comment?: string;
+  deliveryTimestamp?: Date;
+  customerLocation?: {
+    latitude: string;
+    longitude: string;
+  };
+  driverLastKnownLocation?: {
+    latitude: string;
+    longitude: string;
+    receivedAt: Date;
+  };
+}
