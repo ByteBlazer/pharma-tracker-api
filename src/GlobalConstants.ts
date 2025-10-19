9f55aa57f97177c4efd76854ecef4a792ce974fe
Sujith George <sujith@byteblazer.com>
2025-10-19 17:32:30 +0530

    v3

--- src/GlobalConstants.ts
+++ src/GlobalConstants.ts
@@ -6,7 +6,7 @@ export class GlobalConstants {
   static readonly SEND_SMS_URL_TEMPLATE =
     "https://2factor.in/API/R1/?module=TRANS_SMS&apikey={apikey}&to={recipientMobileNumber}&from=BTBLZR&templatename={smsTemplateName}";
   static readonly SMS_OTP_TEMPLATE = "PHARMA_OTP_V6";
-  static readonly SMS_TRACK_TEMPLATE = "PHARMA_TRACK_V2";
+  static readonly SMS_TRACK_TEMPLATE = "PHARMA_TRACK_V3";
   static readonly SMS_API_KEY = "397be28f-2352-11ec-a13b-0200cd936042";
 
   // Tracking SMS Control - Set to true to enable tracking SMS sending in local Windows environment
